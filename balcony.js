9a6b875bb7d1533a5881a0395052f810a02770ec
Swam127 <76239041+Swam127@users.noreply.github.com>
2021-05-26 20:56:52 +0530

    Add files via upload

---
+++ balcony.js
@@ -0,0 +1,58 @@
+img = "";
+status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('balcony.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Object Detecting";
+}
+
+function modelLoaded()
+{
+    console.log("COCOSSD is Intialized");
+    status = true;
+    detector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+
+    if(status != "")
+    {
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill("#FF0000");
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function back()
+{
+    window.location = "index.html";
+}
